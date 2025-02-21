b373b531c3f48a9955de61b503446d6ff374904e
Alice Oh <5771544+helloaliceoh@users.noreply.github.com>
2025-02-21 10:11:40 -0800

    update vite config file to add subpath

--- workspace/react_template/vite.config.js
+++ workspace/react_template/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/bring-foursquare-back/',
   optimizeDeps: {
     include: ['react', 'react-dom', 'tailwindcss']
   }
